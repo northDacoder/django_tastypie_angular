3fe47ba91477de0c728a4860039e1b60682ba139
John Ryan <jcryan85@gmail.com>
2014-03-12 13:19:20 -0700

    changed order of files in html header

--- registar/static/js/controllers/indexController.js
+++ registar/static/js/controllers/indexController.js
@@ -2,6 +2,7 @@ function indexController($scope, $http) {
 
     $http.get('api/v1/student/?format=json')
         .success(function(students){
+            console.log(students);
             $scope.students = students.objects;
     });
 
