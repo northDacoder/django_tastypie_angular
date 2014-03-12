8a10eedc1353ddececd42915563f4f45026cd93c
John Ryan <jcryan85@gmail.com>
2014-03-12 15:39:21 -0700

    added studentController

--- registar/static/js/app.js
+++ registar/static/js/app.js
@@ -4,6 +4,7 @@ registar.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/', { templateUrl: '/static/views/index.html', controller: 'indexController'})
         .when('/add/student/', { templateUrl: '/static/views/add_student.html', controller: 'addStudentController'})
+        .when('/student/', { templateUrl: '/static/views/student.html', controller: 'studentController'})
         .when('/secret/', { templateUrl: '/static/views/secret.html', controller: 'addStudentController' })
         .when('/add/project/', { templateUrl: '/static/views/add_project.html', controller: 'addProjectController' })
         .otherwise({ redirectTo: '/' });
