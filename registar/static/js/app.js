d040d0ee4e3322d100cc005b125c4191b00d2b99
John Ryan <jcryan85@gmail.com>
2014-03-12 13:22:14 -0700

    added new route in app.js

--- registar/static/js/app.js
+++ registar/static/js/app.js
@@ -3,5 +3,6 @@ var registar = angular.module('registar', ['ngRoute']);
 registar.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/', { templateUrl: '/static/views/index.html', controller: 'indexController'})
+        .when('/', { templateUrl: '/static/views/add_student.html', controller: 'addStudentController'})
         .otherwise({ redirectTo: '/' });
 }]);
