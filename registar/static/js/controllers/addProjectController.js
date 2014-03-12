acae7d9b813c56a4e0f43f88714d9b9948b44f8b
John Ryan <jcryan85@gmail.com>
2014-03-12 16:24:33 -0700

    changed GET & POST

--- registar/static/js/controllers/addProjectController.js
+++ registar/static/js/controllers/addProjectController.js
@@ -1,6 +1,10 @@
 function addProjectController($scope, $http, $location) {
 
-    $scope.get('/api/v1/student/?format=json')
+    $scope.get('/api/v1/student/?format=json').success(function(data){
+        console.log(data);
+    }).error(function(data){
+        console.log(data);
+    })
 
     $scope.submitForm = function() {
         $http.post('/api/v1/student/?format=json')
