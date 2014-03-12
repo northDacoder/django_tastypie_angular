9ade6cf08ca460f5d6bbeeb517d58e57bffb540f
John Ryan <jcryan85@gmail.com>
2014-03-12 11:08:53 -0700

    changes to routes

--- registar/static/js/app.js
+++ registar/static/js/app.js
@@ -3,4 +3,5 @@ var registar = angular.module('registar', ['ngRoute']);
 registar.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/', { templateUrl: '/static/views/index.html', controller: 'indexController'});
+        .otherwise({ redirectTo: '/' });
 }]);
