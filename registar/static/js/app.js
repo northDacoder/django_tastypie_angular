094116016efe805d8bd66eecf789f092c289e322
John Ryan <jcryan85@gmail.com>
2014-03-12 16:28:18 -0700

    changes to studentController.js

--- registar/static/js/app.js
+++ registar/static/js/app.js
@@ -1,10 +1,10 @@
-var registar = angular.module('registar', ['ngRoute', 'ngResource']);
+var registar = angular.module('registar', ['ngRoute']);
 
 registar.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider
         .when('/', { templateUrl: '/static/views/index.html', controller: 'indexController'})
         .when('/add/student/', { templateUrl: '/static/views/add_student.html', controller: 'addStudentController'})
-        .when('/student', { templateUrl: '/static/views/student.html', controller: 'studentController'})
+        .when('/student/:id', { templateUrl: '/static/views/student.html', controller: 'studentController'})
         .when('/secret', { templateUrl: '/static/views/secret.html', controller: 'addStudentController' })
         .when('/add/project/', { templateUrl: '/static/views/add_project.html', controller: 'addProjectController' });
 }]);
